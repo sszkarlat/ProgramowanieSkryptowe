ab36cd508d9dc9341a3065c2123aee308b632540
sszkarlat <sszkarlat@student.agh.edu.pl>
2023-10-07 22:17:19 +0200

    Modyfikacja zadania domowego z lab1.

--- lab1/pytest_skrypt.py
+++ lab1/pytest_skrypt.py
@@ -1,14 +1,13 @@
-import sys
+from skrypt import display, run, move_description
 import pytest
-from skrypt import display, run, moves_description
 
 
-# Testy dla funkcji display
+# Testy pytest dla funkcji display
 def test_display_without_index(capsys):
-    args = ["arg1", "arg2", "arg3"]
+    args = ["1", "2", "3"]
     display(args, False)
     out, err = capsys.readouterr()
-    assert out == "Start\narg1\narg2\narg3\nStop\n"
+    assert out == "Start\n1\n2\n3\nStop\n"
 
 
 def test_display_with_index(capsys):
@@ -18,7 +17,7 @@ def test_display_with_index(capsys):
     assert out == "Start\nargs[0] = arg1\nargs[1] = arg2\nargs[2] = arg3\nStop\n"
 
 
-# Testy dla funkcji run
+# Testy pytest dla funkcji run
 def test_run():
     moves = ["f", "b", "l", "r", "x"]
     expected_result = [
@@ -27,16 +26,16 @@ def test_run():
         "Zwierzak skręca w lewo",
         "Zwierzak skręca w prawo",
     ]
-    assert run(moves, moves_description) == expected_result
+    assert run(moves, move_description) == expected_result
 
 
 def test_run_with_empty_moves():
     moves = []
     expected_result = []
-    assert run(moves, moves_description) == expected_result
+    assert run(moves, move_description) == expected_result
 
 
 def test_run_with_invalid_moves():
     moves = ["x", "y", "z"]
     expected_result = []
-    assert run(moves, moves_description) == expected_result
+    assert run(moves, move_description) == expected_result
