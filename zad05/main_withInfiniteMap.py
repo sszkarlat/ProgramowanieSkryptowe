c3491f4e1487246c5989962f3781ebe125b8b549
sszkarlat <sszkarlat@student.agh.edu.pl>
2023-11-14 20:09:12 +0100

    Update file

--- zad05/main_withInfiniteMap.py
+++ zad05/main_withInfiniteMap.py
@@ -4,17 +4,7 @@ from controller import Simulation, OptionsParser
 from model.map import InfiniteMap  
 
 directions: list[MoveDirection] = OptionsParser.parse(sys.argv[1:])
-positions: list[Vector2d] = [
-    Vector2d(2, 2), 
-    Vector2d(3, 4),
-    Vector2d(4, 8),
-    Vector2d(7, 5),
-    Vector2d(2, 3),
-    Vector2d(3, 2), 
-    Vector2d(5, 5),
-    Vector2d(10, 10),
-    Vector2d(17, 15),
-    ]
+positions: list[Vector2d] = [Vector2d(2, 2), Vector2d(3, 4)]
 # Poprzednio
 # simulation: Simulation = Simulation(directions, positions)
 # Obecnie
