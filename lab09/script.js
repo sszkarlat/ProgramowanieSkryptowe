eb5113a90c76977ad896f46b57e1e086ddc6a24f
sszkarlat <sszkarlat@student.agh.edu.pl>
2023-12-14 12:03:51 +0100

    Update script.js file

--- lab09/script.js
+++ lab09/script.js
@@ -1,42 +1,68 @@
-function logUserData() {
-    var userInput = document.getElementById("userName").value;
-    // console.log(parseInt(userInput));
+// function logUserData() {
+//     var userInput = document.getElementById("userName").value;
+//     // console.log(parseInt(userInput));
 
-    flag = true;
-    for (const char of userInput) {
-        if (!isNaN(parseInt(char))) {
-            console.group("Error-group");
-            console.error("Wprowadziłeś liczbę!");
-            console.groupEnd("Error-group");
-            flag = false
-            break;
-        }
-
-    }
-    if (flag === true) {
-        console.group("Pass-group");
-        console.log("Wprowadzone dane", userInput)
-        console.groupEnd("Pass-group");
-    }
-
-
-}
+//     flag = true;
+//     for (const char of userInput) {
+//         if (!isNaN(parseInt(char))) {
+//             console.group("Error-group");
+//             console.error("Wprowadziłeś liczbę!");
+//             console.groupEnd("Error-group");
+//             flag = false
+//             break;
+//         }
+//     }
+//     if (flag === true) {
+//         console.group("Pass-group");
+//         console.log("Wprowadzone dane", userInput)
+//         console.groupEnd("Pass-group");
+//     }
+// }
 
 // Bardzo dobre do przeczytania: https://web.dev/articles/indexeddb?hl=pl
 const request = indexedDB.open("dataBase", 1);
 let dataBase;
 
+request.onerror = (event) => {
+    window.alert("Błąd otwierania bazy danych:", event.target.errorCode);
+};
+
 request.onupgradeneeded = (event) => {
     dataBase = event.target.result;
 
-    const productsObjectStore = dataBase.createObjectStore("Products", { keyPath: "id", autoIncrement: true });
-    productsObjectStore.createIndex("name", "name", { unique: false });
-    productsObjectStore.createIndex("quantity", "quantity", { unique: false });
-    productsObjectStore.createIndex("price", "price", { unique: false });
+    if (!dataBase.objectStoreNames.contains("Products")) {
+        const productsObjectStore = dataBase.createObjectStore("Products", { keyPath: "id", autoIncrement: true });
+        productsObjectStore.createIndex("name", "name", { unique: false });
+        productsObjectStore.createIndex("quantity", "quantity", { unique: false });
+        productsObjectStore.createIndex("price", "price", { unique: false });
 
-    const customersObjectStore = dataBase.createObjectStore("Customers", { keyPath: "id", autoIncrement: true });
-    customersObjectStore.createIndex("firstName", "firstName", { unique: false });
-    customersObjectStore.createIndex("lastName", "lastName", { unique: false });
+        const productsData = [
+            { name: "BMW", quantity: 10, price: 800000 },
+            { name: "Fiat", quantity: 25, price: 50000 },
+            { name: "Mercedes", quantity: 15, price: 1000000 },
+            { name: "Volvo", quantity: 8, price: 400000 }
+        ];
+
+        productsData.forEach((product) => {
+            productsObjectStore.add(product);
+        });
+    }
+
+    if (!dataBase.objectStoreNames.contains("Customers")) {
+        const customersObjectStore = dataBase.createObjectStore("Customers", { keyPath: "id", autoIncrement: true });
+        customersObjectStore.createIndex("firstName", "firstName", { unique: false });
+        customersObjectStore.createIndex("lastName", "lastName", { unique: false });
+
+        const customersData = [
+            { firstName: "Jan", lastName: "Kowalski" },
+            { firstName: "Adam", lastName: "Nowak" },
+            { firstName: "Monika", lastName: "Miss" }
+        ];
+
+        customersData.forEach((customer) => {
+            customersObjectStore.add(customer);
+        });
+    }
 };
 
 request.onsuccess = (event) => {
@@ -45,37 +71,16 @@ request.onsuccess = (event) => {
     const productsTransaction = dataBase.transaction("Products", "readwrite");
     const productsObjectStore = productsTransaction.objectStore("Products");
 
-    //     const productsData = [
-    //         { name: "BMW", quantity: 10, price: 800000 },
-    //         { name: "Fiat", quantity: 25, price: 50000 },
-    //         { name: "Mercedes", quantity: 15, price: 1000000 },
-    //         { name: "Volvo", quantity: 8, price: 400000 }
-    //     ];
+    const getAllProducts = productsObjectStore.getAll();
 
-    //     productsData.forEach((product) => {
-    //         productsObjectStore.add(product);
-    //     });
+    getAllProducts.onsuccess = () => {
+        const productsData = getAllProducts.result;
+        console.log("Dane produktów:", productsData);
+    };
+};
 
-    //     productsTransaction.oncomplete = () => {
-    //         console.log("Zainicjalizowano dane produktów.");
-    //     };
-
-    const customersTransaction = dataBase.transaction("Customers", "readwrite");
-    const customersObjectStore = customersTransaction.objectStore("Customers");
-
-    //     const customersData = [
-    //         { firstName: "Jan", lastName: "Kowalski" },
-    //         { firstName: "Adam", lastName: "Nowak" },
-    //         { firstName: "Monika", lastName: "Miss" }
-    //     ];
-
-    //     customersData.forEach((customer) => {
-    //         customersObjectStore.add(customer);
-    //     });
-
-    //     customersTransaction.oncomplete = () => {
-    //         console.log("Zainicjalizowano dane klientów.");
-    //     };
+request.onerror = (event) => {
+    console.error("Błąd otwierania bazy danych:", event.target.errorCode);
 };
 
 function executeCommand() {
@@ -117,11 +122,7 @@ function sellProduct(commandParts) {
     const getProductRequest = productsObjectStore.index("name").get(productName);
     const getCustomerRequest = customersObjectStore.index("lastName").get(customerLastName);
 
-    transaction.oncomplete = () => {
-        console.group("Sprzedaż");
-        console.log(`Sprzedano ${quantityToSell} sztuk produktu "${productName}" klientowi "${customerFirstName} ${customerLastName}"`);
-        console.groupEnd();
-    };
+
 
     getProductRequest.onsuccess = (event) => {
         const product = event.target.result;
@@ -132,8 +133,14 @@ function sellProduct(commandParts) {
                 product.quantity -= quantityToSell;
                 const updateProductRequest = productsObjectStore.put(product);
 
+                transaction.oncomplete = () => {
+                    console.group("Sprzedaż");
+                    console.log(`Sprzedano ${quantityToSell} sztuk produktu "${productName}" klientowi "${customerFirstName} ${customerLastName}"`);
+                    console.groupEnd();
+                };
+
+
                 updateProductRequest.onerror = (error) => {
-                    // console.error("Błąd podczas aktualizacji ilości produktu:", error);
                     window.alert("Błąd podczas aktualizacji ilości produktu:", error);
                 };
 
@@ -148,31 +155,27 @@ function sellProduct(commandParts) {
                         const updateCustomerRequest = customersObjectStore.put(customer);
 
                         updateCustomerRequest.onerror = (error) => {
-                            // console.error("Błąd podczas aktualizacji historii zakupów klienta:", error);
                             window.alert("Błąd podczas aktualizacji historii zakupów klienta:", error);
                         };
                     } else {
-                        // console.error(`Nie znaleziono klienta o nazwisku "${customerLastName}"`);
-                        window.alert(`Nie znaleziono klienta o nazwisku "${customerLastName}"`);
+                        window.alert(`Nie znaleziono klienta o nazwisku ${customerLastName}`);
                     }
                 };
             } else {
-                // console.warn(`Produkt "${productName}" niedostępny lub brak wystarczającej ilości w magazynie.`);
-                window.alert(`Produkt "${productName}" niedostępny lub brak wystarczającej ilości w magazynie.`);
+                window.alert(`Produkt: ${productName} chwilowo niedostępny.`);
             }
+        } else if (quantityToSell === 0) {
+            window.alert("Nie wolno kupić zerowej liczby sztuk");
         } else {
-            // console.warn("Nie wolno kupować ujemnej ilości sztuk.");
-            window.alert("Nie wolno kupować ujemnej ilości sztuk.");
+            window.alert("Nie wolno kupić ujemnej liczby sztuk.");
         }
     };
 
     getProductRequest.onerror = (error) => {
-        // console.error("Błąd podczas pobierania danych produktu:", error);
         window.alert("Błąd podczas pobierania danych produktu:", error);
     };
 
     getCustomerRequest.onerror = (error) => {
-        // console.error("Błąd podczas pobierania danych klienta:", error);
         window.alert("Błąd podczas pobierania danych klienta:", error);
     };
 }
@@ -195,19 +198,19 @@ function displayCustomers() {
     };
 
     getAllCustomersRequest.onerror = (error) => {
-        // console.error("Błąd podczas pobierania listy klientów:", error);
         window.alert("Błąd podczas pobierania listy klientów:", error);
     };
 }
 
 function displayPurchaseHistory(commandParts) {
-    const customerLastName = commandParts[1];
+    const customerFirstName = commandParts[1];
+    const customerLastName = commandParts[2];
 
     const transaction = dataBase.transaction(["Products", "Customers"], "readonly");
-    const productsObjectStore = transaction.objectStore("Products");
+    // const productsObjectStore = transaction.objectStore("Products");
     const customersObjectStore = transaction.objectStore("Customers");
 
-    const getCustomerRequest = customersObjectStore.index("lastName").get(customerLastName);
+    const getCustomerRequest = (customersObjectStore.index("firstName").get(customerFirstName), customersObjectStore.index("lastName").get(customerLastName));
 
     getCustomerRequest.onsuccess = (event) => {
         const customer = event.target.result;
@@ -215,7 +218,7 @@ function displayPurchaseHistory(commandParts) {
         if (customer) {
             const purchaseHistory = customer.purchaseHistory || [];
             console.group("Historia zakupów");
-            console.log(`Historia zakupów klienta "${customer.firstName} ${customer.lastName}":`);
+            console.log(`Historia zakupów klienta: ${customer.firstName} ${customer.lastName}:`);
             let totalCost = 0;
 
             purchaseHistory.forEach((purchase) => {
@@ -223,19 +226,17 @@ function displayPurchaseHistory(commandParts) {
                 const cost = product.price * purchase.quantity;
                 totalCost += cost;
 
-                console.log(`- ${product.name}, ilość: ${purchase.quantity}, cena: ${cost}`);
+                console.log(`- ${product.name}, liczba sztuk: ${purchase.quantity}, cena: ${cost}`);
             });
 
             console.log(`Sumaryczna cena zakupów: ${totalCost}`);
             console.groupEnd();
         } else {
-            // console.error(`Nie znaleziono klienta o nazwisku "${customerLastName}"`);
-            window.alert(`Nie znaleziono klienta o nazwisku "${customerLastName}"`);
+            window.alert(`Nie znaleziono klienta: ${customerFirstName} ${customerLastName}`);
         }
     };
 
     getCustomerRequest.onerror = (error) => {
-        // console.error("Błąd podczas pobierania danych klienta:", error);
         window.alert("Błąd podczas pobierania danych klienta:", error);
     };
 }
@@ -258,7 +259,6 @@ function displayInventory() {
     };
 
     getAllProductsRequest.onerror = (error) => {
-        // console.error("Błąd podczas pobierania stanu magazynu:", error);
         window.alert("Błąd podczas pobierania stanu magazynu:", error);
     };
 }
