6c1731292aaa706f8b59766f6e2521a38d116572
sszkarlat <sszkarlat@student.agh.edu.pl>
2024-01-05 10:46:13 +0100

    laby task - done

--- lab11/server.js
+++ lab11/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const clientRouter = require("./routes/client");
 const adminRouter = require("./routes/admin");
 
 const app = express();
-
 const port = 8000;
+app.locals.pretty = app.get("env") === "development"; // The resulting HTML code will be indented in the development environment
+app.use(morgan("dev"));
+
 app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use("/client", clientRouter);
